1f21607fd4d879a76ed749af110e902e8e849cea
alisson-b <59566887+alisson-b@users.noreply.github.com>
2020-10-13 20:56:35 -0300

    Create AreaRetangulo.java

---
+++ 13/AreaRetangulo.java
@@ -0,0 +1,10 @@
+public class AreaRetangulo {
+    public static void main(String[] arg) {
+        double base, altura, AreaRetangulo;
+        base = 16.98;
+        altura = 12.77;
+        AreaRetangulo = base * altura;
+        System.out.println("A área do retangulo é de " + AreaRetangulo + " metros quadrados!");
+        
+    } 
+}
