1c4c1d9097105353e357bcb12a19d94983d928cd
alisson-b <59566887+alisson-b@users.noreply.github.com>
2020-10-13 20:39:43 -0300

    Create Multiplica.java

---
+++ 13/Multiplica.java
@@ -0,0 +1,9 @@
+public class Multiplica {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 50;
+        int z = x*y;
+        System.out.println(z);
+    }
+    
+}
